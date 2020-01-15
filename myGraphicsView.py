8f92e9b512aaad94a29e72dbd30db36796bc4c3b
ZalmanHack <dobrenko44@gmail.com>
2020-01-15 22:17:44 +0200

    ver 0.2 3d срез готов. Нет полигонов и ребер

--- myGraphicsView.py
+++ myGraphicsView.py
@@ -37,45 +37,42 @@ class MyGraphicView(QGraphicsView):
         self.transform4D = Transformations4D()
 
     def initFunctions(self):
-        self.figure = Figures.tesseract
-
-        self.points2D: list = []
-        self.points3D: list = []
-        self.points4D: list = self.figure['points']
+        self.figure4D = Figures().tesseract
+        self.figure3D = None
+        self.figure2D = None
+        self.coordinate_w = 0
         self.distance = 3
         self.angle = 0.01
 
-    def connect3D(self, i = 0, j = 0, points: list = ()):
+    def connect(self, i = 0, j = 0, points: list = ()):
         self.scene.addLine(points[i][0], points[i][1],
                            points[j][0], points[j][1], self.pen)
-
-    def connect4D(self, i = 0, j = 0, points: list = ()):
-        self.scene.addLine(points[i][0], points[i][1],
-                           points[j][0], points[j][1], self.pen)
-
     def draw(self):
         self.scene.clear()
-        self.points3D = self.transform4D.projection_3D(self.points4D, self.distance)
-        self.points3D = self.transform3D.rotate_x(self.points3D, 90)
-        self.points2D = self.transform3D.projection_2D(self.points3D, self.distance)
-        self.points2D = self.transform3D.scale(self.points2D, 500)
+        self.figure4D = self.transform4D.rotate_zw(self.figure4D, 0.1)  # поворот
+        self.figure4D = self.transform4D.rotate_xz(self.figure4D, 0.1)  # поворот
 
+        self.figure3D = self.transform4D.projection_3D(self.figure4D, self.distance, self.coordinate_w)  # проекция
+        self.figure3D = self.transform3D.rotate_y(self.figure3D, 50)  # поворот
+
+        self.figure2D = self.transform3D.projection_2D(self.figure3D, self.distance)  # проекция
+        print(self.figure3D['points'])
+        self.figure2D = self.transform3D.scale(self.figure2D, 500)
+        """
         self.pen = QPen(Qt.black)
         self.pen.setWidth(3)
         for item in self.figure['edges']:
-            self.connect4D(item[0], item[1], self.points2D)
+            self.connect(item[0], item[1], self.points2D)
+        """
         diameter = 10
         self.pen.setWidth(3)
-        for point in self.points2D:
-            self.scene.addEllipse(point[0] - diameter/2, point[1] - diameter/2, diameter, diameter, QPen(Qt.NoPen), QBrush(Qt.red))
+        for point in self.figure2D['points']:
+            self.scene.addEllipse(point[0] - diameter/2, point[1] - diameter/2, diameter, diameter, QPen(Qt.NoPen), QBrush(Qt.black))
+
+        self.coordinate_w += 0.0005
 
     def timeout(self):
-        #self.points4D = self.transform4D.rotate_zw(self.points4D, 0.1)
-        #self.points4D = self.transform4D.rotate_xy(self.points4D, 0.1)
-        self.points4D = self.transform4D.rotate_zw(self.points4D, 0.1)
-        self.points4D = self.transform4D.rotate_xy(self.points4D, 0.1)
         self.draw()
-
 
 
 if __name__ == "__main__":
--- transformations.py
+++ transformations.py
@@ -1,6 +1,7 @@
 from math import *
 import numpy as np
 
+
 class Transformations3D():
     def _dot(self, points3D: list = (), matrix3D: list = ()):
         points = np.array(points3D)
@@ -12,36 +13,39 @@ class Transformations3D():
                     result.append(matrix.dot(row_XYZ.reshape(3, 1)).reshape(1, 3)[0])
             return result
 
-    def rotate_x(self, points3D: list = (), angle=1.0):
+    def rotate_x(self, figure3D: dict = (), angle=1.0):
         matrix = [
             [1, 0, 0],
             [0, cos(radians(angle)), -sin(radians(angle))],
             [0, sin(radians(angle)), cos(radians(angle))]
         ]
-        return self._dot(points3D, matrix)
+        figure3D['points'] = self._dot(figure3D['points'], matrix)
+        return figure3D
 
-    def rotate_y(self, points3D: list = (), angle=1.0):
+    def rotate_y(self, figure3D: dict = (), angle=1.0):
         matrix = [
             [cos(radians(angle)), 0, -sin(radians(angle))],
             [0, 1, 0],
             [sin(radians(angle)), 0, cos(radians(angle))]
         ]
-        return self._dot(points3D, matrix)
+        figure3D['points'] = self._dot(figure3D['points'], matrix)
+        return figure3D
 
-    def rotate_z(self, points3D: list = (), angle=1.0):
+    def rotate_z(self, figure3D: dict = (), angle=1.0):
         matrix = [
             [cos(radians(angle)), -sin(radians(angle)), 0],
             [sin(radians(angle)), cos(radians(angle)), 0],
             [0, 0, 1]
         ]
-        return self._dot(points3D, matrix)
+        figure3D['points'] = self._dot(figure3D['points'], matrix)
+        return figure3D
 
-    def projection_2D(self, points3D: list = (), distance=1.0):
+    def projection_2D(self, figure3D: dict = (), distance=1.0):
         projection = np.array([
             [0., 0., 0.],
             [0., 0., 0.]
         ], float)
-        array = np.array(points3D)
+        array = np.array(figure3D['points'])
         if array is not None:
             result: list = []
             if len(array.shape) == 2 and array.shape[1] == 3:
@@ -49,12 +53,15 @@ class Transformations3D():
                     projection[0][0] = 1.0 / (distance - row_XYZ[2])
                     projection[1][1] = 1.0 / (distance - row_XYZ[2])
                     result.append(projection.dot(row_XYZ.reshape(3, 1)).reshape(1, 2)[0])
-            return result
+            figure3D['points'] = result
+            return figure3D
 
-    def scale(self, points: list = (), value = 1.0):
-        array = np.array(points)
+    def scale(self, figure: dict = (), value=1.0):
+        array = np.array(figure['points'])
         array = array.dot(value)
-        return array.tolist()
+        figure['points'] = array.tolist()
+        return figure
+
 
 class Transformations4D():
     def _dot(self, points4D: list = (), matrix4D: list = ()):
@@ -67,78 +74,123 @@ class Transformations4D():
                     result.append(matrix.dot(row_XYZ.reshape(4, 1)).reshape(1, 4)[0])
             return result
 
-    def rotate_xw(self, points4D: list = (), angle=1.0):
+    def rotate_xw(self, figure4D: dict = (), angle=1.0):
         matrix = [
             [1, 0, 0, 0],
             [0, cos(radians(angle)), -sin(radians(angle)), 0],
             [0, sin(radians(angle)), cos(radians(angle)), 0],
             [0, 0, 0, 1]
         ]
-        return self._dot(points4D, matrix)
+        figure4D['points'] = self._dot(figure4D['points'], matrix)
+        return figure4D
 
-    def rotate_yw(self, points4D: list = (), angle=1.0):
+    def rotate_yw(self, figure4D: dict = (), angle=1.0):
         matrix = [
             [cos(radians(angle)), 0, -sin(radians(angle)), 0],
             [0, 1, 0, 0],
             [sin(radians(angle)), 0, cos(radians(angle)), 0],
             [0, 0, 0, 1]
         ]
-        return self._dot(points4D, matrix)
+        figure4D['points'] = self._dot(figure4D['points'], matrix)
+        return figure4D
 
-    def rotate_zw(self, points4D: list = (), angle=1.0):
+    def rotate_zw(self, figure4D: dict = (), angle=1.0):
         matrix = [
             [cos(radians(angle)), -sin(radians(angle)), 0, 0],
             [sin(radians(angle)), cos(radians(angle)), 0, 0],
             [0, 0, 1, 0],
             [0, 0, 0, 1]
         ]
-        return self._dot(points4D, matrix)
+        figure4D['points'] = self._dot(figure4D['points'], matrix)
+        return figure4D
 
-    def rotate_xy(self, points4D: list = (), angle=1.0):
+    def rotate_xy(self, figure4D: dict = (), angle=1.0):
         matrix = [
             [1, 0, 0, 0],
             [0, 1, 0, 0],
             [0, 0, cos(radians(angle)), -sin(radians(angle))],
             [0, 0, sin(radians(angle)), cos(radians(angle))],
         ]
-        return self._dot(points4D, matrix)
+        figure4D['points'] = self._dot(figure4D['points'], matrix)
+        return figure4D
 
-    def rotate_xz(self, points4D: list = (), angle=1.0):
+    def rotate_xz(self, figure4D: dict = (), angle=1.0):
         matrix = [
             [1, 0, 0, 0],
             [0, cos(radians(angle)), 0, sin(radians(angle))],
             [0, 0, 1, 0],
             [0, -sin(radians(angle)), 0, cos(radians(angle))],
         ]
-        return self._dot(points4D, matrix)
+        figure4D['points'] = self._dot(figure4D['points'], matrix)
+        return figure4D
 
-    def rotate_yz(self, points4D: list = (), angle=1.0):
+    def rotate_yz(self, figure4D: dict = (), angle=1.0):
         matrix = [
             [cos(radians(angle)), 0, 0, sin(radians(angle))],
             [0, 1, 0, 0],
             [0, 0, 1, 0],
             [-sin(radians(angle)), 0, 0, cos(radians(angle))],
         ]
-        return self._dot(points4D, matrix)
+        figure4D['points'] = self._dot(figure4D['points'], matrix)
+        return figure4D
 
-    def projection_3D(self, points4D: list = (), distance=1.0):
-        projection = np.array([
-            [0., 0., 0., 0],
-            [0., 0., 0., 0],
-            [0., 0., 0., 0]
-        ], float)
-        array = np.array(points4D)
-        if array is not None:
-            result: list = []
-            if len(array.shape) == 2 and array.shape[1] == 4:
-                for row_XYZW in array:
-                    projection[0][0] = 1.0 / (distance - row_XYZW[3])
-                    projection[1][1] = 1.0 / (distance - row_XYZW[3])
-                    projection[2][2] = 1.0 / (distance - row_XYZW[3])
-                    result.append(projection.dot(row_XYZW.reshape(4, 1)).reshape(1, 3)[0])
-            return result
+    def projection_3D(self, figure4D: dict = (), distance=1.0, coordinate_w=0):
+        """
+                projection = np.array([
+                    [0., 0., 0., 0],
+                    [0., 0., 0., 0],
+                    [0., 0., 0., 0]
+                ], float)
+                array = np.array(figure4D['points'])
+                if array is not None:
+                    result: list = []
+                    if len(array.shape) == 2 and array.shape[1] == 4:
+                        for row_XYZW in array:
+                            projection[0][0] = 1.0 / (distance - row_XYZW[3])
+                            projection[1][1] = 1.0 / (distance - row_XYZW[3])
+                            projection[2][2] = 1.0 / (distance - row_XYZW[3])
+                            result.append(projection.dot(row_XYZW.reshape(4, 1)).reshape(1, 3)[0])
+                    return result
+        """
+        points3D = []
+        polygons3D = []  # НЕ РЕАЛИЗОВАНО !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        points4D = figure4D['points']
+        polygons4D = figure4D['polygons']
+        # перебор полигонов и поиск тех, которые находятся на искомой координате
+        for polygon in polygons4D:
+            # перебор точек полигона (в контексте линий)
+            if len(polygon) > 2:
+                index = 0
+                while index < len(polygon):
+                    # запоминаем W'ки точек отрезка
+                    poly1 = points4D[polygon[index]]
+                    if index + 1 == len(polygon):
+                        poly2 = points4D[polygon[0]]
+                    else:
+                        poly2 = points4D[polygon[index + 1]]
+                    # если первый > второго, то меняем местами, (для правильного диапазона)
+                    if poly1[3] > poly2[3]:
+                        poly1, poly2 = poly2, poly1
+                    # проверяем, входит ли искомая координата в искомый диапазон
+                    if coordinate_w > poly1[3] and coordinate_w <= poly2[3]:
+                        # вычисляем координаты новой точки, которая лежит на данном отрезке
+                        # получем отношение получившихся 2х отрезков (по W координате, потому что знаем только ее)
+                        # получаем альфу отношения
+                        alfa = fabs(coordinate_w - poly1[3]) / fabs(coordinate_w - poly2[3])
+                        # получаем координаты новой точки
+                        x = (poly1[0] + alfa * poly2[0]) / (1 + alfa)
+                        y = (poly1[1] + alfa * poly2[1]) / (1 + alfa)
+                        z = (poly1[2] + alfa * poly2[2]) / (1 + alfa)
+                        # добавляем новую точку в 3Д массив
+                        if [x, y, z] not in points3D:
+                            points3D.append([x, y, z])
+                    index += 1
+        print(len(points3D))
+        print("------------------------------------------")
+        return {'points': points3D, 'polygons': polygons3D}
 
-    def scale(self, points: list = (), value = 1.0):
-        array = np.array(points)
+    def scale(self, figure: dict = (), value=1.0):
+        array = np.array(figure['points'])
         array = array.dot(value)
-        return array.tolist()
+        figure['points'] = array.tolist()
+        return figure
